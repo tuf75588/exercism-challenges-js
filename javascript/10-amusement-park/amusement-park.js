5a4fe2e83b34d8ec431ea706a6631f0ac9defa83
Andrew Davis <atd285@gmail.com>
2022-01-04 20:30:45 -0500

    part one challenge exercise 10

---
+++ javascript/10-amusement-park/amusement-park.js
@@ -0,0 +1,65 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  return { name, age, ticketId };
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+  return { ...visitor, ticketId: null };
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+  if (ticketId in tickets === false) return 'unknown ticket id';
+  const keys = Object.keys(tickets);
+  const status = keys.map((keyId) => {
+    if (keyId === ticketId) {
+      if (tickets[keyId] === null) return 'not sold';
+      return `sold to ${tickets[keyId]}`;
+    }
+  });
+  return status.join('');
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+  throw new Error('Please implement the simpleTicketStatus function.');
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+export function gtcVersion(visitor) {
+  throw new Error('Please implement the gtcVersion function.');
+}
