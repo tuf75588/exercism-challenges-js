047336473fee669f624fc1a8843b063755ef8e21
Andrew Davis <atd285@gmail.com>
2022-01-04 22:13:57 -0500

    first solution lucky-numbers problem

---
+++ javascript/11-lucky-numbers/lucky-numbers.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  const [one, two] = [
+    array1.toString().split(',').join(''),
+    array2.toString().split(',').join(''),
+  ];
+
+  return parseInt(one) + parseInt(two);
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean}  whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+  throw new Error('Implement the luckyNumber function');
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+  throw new Error('Implement the errorMessage function');
+}
