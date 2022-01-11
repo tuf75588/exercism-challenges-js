99fa4fd52e16f62f01beec92f0776b212b6091ab
Andrew Davis <atd285@gmail.com>
2022-01-10 22:30:20 -0500

    finish

--- javascript/19-enchantments.js
+++ javascript/19-enchantments.js
@@ -7,80 +7,79 @@
  *
  * @returns {number[]} deck with every card doubled
  */
- export function seeingDouble(deck) {
-  return deck.map((card) => card*2);
+export function seeingDouble(deck) {
+  return deck.map((card) => card * 2);
 }
 
 /**
-*  Creates triplicates of every 3 found in the deck.
-*
-* @param {number[]} deck
-*
-* @returns {number[]} deck with triplicate 3s
-*/
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
 export function threeOfEachThree(deck) {
-  return deck.flatMap(card => card === 3 ? [3,3,3] : card);  
+  return deck.flatMap((card) => (card === 3 ? [3, 3, 3] : card));
 }
 
 /**
-* Extracts the middle two cards from a deck.
-* Assumes a deck is always 10 cards.
-*
-* @param {number[]} deck of 10 cards
-*
-* @returns {number[]} deck with only two middle cards
-*/
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
 export function middleTwo(deck) {
-return deck.slice(4,6);
-
+  return deck.slice(4, 6);
 }
 
 /**
-* Moves the outside two cards to the middle.
-*
-* @param {number[]} deck with 10 cards
-*
-* @returns {number[]} transformed deck
-*/
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with 10 cards
+ *
+ * @returns {number[]} transformed deck
+ */
 
 export function sandwichTrick(deck) {
-const first = deck[0];
-const last = deck[deck.length - 1];
-
-const middlePair = [...deck.slice(1, deck.length / 2), first, last, ...deck.slice(deck.length / 2, deck.length / 2 + 2)];
-return middlePair
-
+  const first = deck.shift();
+  const last = deck.pop();
+  const middleIndex = deck.length / 2;
+  deck.splice(middleIndex, 0, last, first);
+  return deck;
 }
 
 /**
-* Removes every card from the deck except 2s.
-*
-* @param {number[]} deck
-*
-* @returns {number[]} deck with only 2s
-*/
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
 export function twoIsSpecial(deck) {
-throw new Error('Implement the twoIsSpecial function');
+  return deck.filter((x) => x === 2);
 }
 
 /**
-* Returns a perfectly order deck from lowest to highest.
-*
-* @param {number[]} deck shuffled deck
-*
-* @returns {number[]} ordered deck
-*/
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
 export function perfectlyOrdered(deck) {
-throw new Error('Implement the perfectlyOrdered function');
+  deck.sort((a, b) => a - b);
+  return deck;
 }
 
 /**
-* Reorders the deck so that the top card ends up at the bottom.
-*
-* @param {number[]} deck
-*
-* @returns {number[]} reordered deck
-*/
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
 export function reorder(deck) {
-throw new Error('Implement the reorder function');
+  return deck.reverse();
 }
