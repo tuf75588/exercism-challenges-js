29a222ee137254ec8b1ec25b24aca383e9dc8052
Andrew Davis <atd285@gmail.com>
2022-01-23 17:51:13 -0500

    factory sensors

--- javascript/23-ozans-playlist.js
+++ javascript/23-ozans-playlist.js
@@ -1,60 +1,68 @@
 // @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
 
-/**
- * Removes duplicate tracks from a playlist.
- *
- * @param {string[]} playlist
- * @returns {string[]} new playlist with unique entries
- */
-export function removeDuplicates(playlist) {
-  return [...new Set(playlist)];
+export class ArgumentError extends Error {}
+
+export class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
 }
 
 /**
- * Checks whether a playlist includes a track.
+ * Check if the humidity level is not too high.
  *
- * @param {string[]} playlist
- * @param {string} track
- * @returns {boolean} whether the track is in the playlist
+ * @param {number} humidityPercentage
+ * @throws {Error}
  */
-export function hasTrack(playlist, track) {
-  return playlist.includes(track);
+export function checkHumidityLevel(humidityPercentage) {
+  if (humidityPercentage > 70) {
+    throw new Error('Too hot!');
+  }
+  return humidityPercentage;
 }
 
 /**
- * Adds a track to a playlist.
+ * Check if the temperature is not too high.
  *
- * @param {string[]} playlist
- * @param {string} track
- * @returns {string[]} new playlist
+ * @param {number|null} temperature
+ * @throws {ArgumentError|OverheatingError}
  */
-export function addTrack(playlist, track) {
-  const addition = playlist.concat(track);
-  return [...new Set(addition)];
+export function reportOverheating(temperature) {
+  const broken = null;
+  if (temperature === broken) {
+    throw new ArgumentError();
+  } else if (temperature > 500) {
+    throw new OverheatingError(temperature);
+  }
+  return temperature;
 }
 
 /**
- * Deletes a track from a playlist.
+ *  Triggers the needed action depending on the result of the machine check.
  *
- * @param {string[]} playlist
- * @param {string} track
- * @returns {string[]} new playlist
+ * @param {{
+ * check: function,
+ * alertDeadSensor: function,
+ * alertOverheating: function,
+ * shutdown: function
+ * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
  */
-export function deleteTrack(playlist, track) {
-  return playlist.filter((t) => t !== track);
-}
-
-/**
- * Lists the unique artists in a playlist.
- *
- * @param {string[]} playlist
- * @returns {string[]} list of artists
- */
-export function listArtists(playlist) {
-  const artistName = playlist.map((s) => s.split('-')[1].trim());
-  return [...new Set(artistName)];
+export function monitorTheMachine(actions) {
+  const { check, alertDeadSensor, alertOverheating, shutdown } = actions;
+  // check the current temp
+  try {
+    check();
+  } catch (error) {
+    const { message } = error;
+    if (error instanceof ArgumentError) {
+      actions.alertDeadSensor();
+    } else if (error instanceof OverheatingError) {
+      const { temperature } = error;
+      temperature > 600 ? actions.shutdown() : actions.alertOverheating();
+    } else {
+      throw error;
+    }
+  }
 }
