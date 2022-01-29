5077aab744b0e1be2dec99ff2caa2297e809e8fb
Andrew Davis <atd285@gmail.com>
2022-01-29 15:13:23 -0500

    resistor color duo

--- javascript/24-resistor-color-duo.js
+++ javascript/24-resistor-color-duo.js
@@ -2,22 +2,22 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const colors = {
-    black: 0,
-    brown: 1,
-    red: 2,
-    orange: 3,
-    yellow: 4,
-    green: 5,
-    blue: 6,
-    violet: 7,
-    grey: 8,
-    white: 9,
-  }
-  
-const decodedValue = (color, color2) => {
-    return Object.keys(colors).map((key,i,a) => {
-       return a.filter(c => c === color || c === color2);
-    })
-  };
-  
+export const colors = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const decodedValue = (colorCodes) => {
+  const [one, two] = colorCodes;
+  const valOne = String(colors[one]);
+  const valTwo = String(colors[two]);
+  return Number(valOne + valTwo);
+};
