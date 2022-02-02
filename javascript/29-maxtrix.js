97c95db14f5e54852ff5f6530906568217d32010
Andrew Davis <atd285@gmail.com>
2022-02-02 14:09:27 -0500

    problem 29 scaffold

---
+++ javascript/29-maxtrix.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(string) {
+    this.string = string.split('\n');
+  }
+
+  get rows() {}
+
+  get columns() {}
+}
