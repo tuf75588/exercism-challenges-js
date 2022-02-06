38d3898ce2b3d3ead611090f8fc0c9d547a7a127
Andrew Davis <atd285@gmail.com>
2022-02-06 14:47:46 -0500

    question cases covered

--- javascript/30-bob.js
+++ javascript/30-bob.js
@@ -4,9 +4,15 @@
 //
 
 export const hey = (message) => {
-    const normal_question = /[a-z]+[?]$/gm;
-    const forceful_question = /\b[A-Z]+[?]$/gm;
-    const all_caps = /(\b[A-Z]+\b){3,}/gm;
-    const silence = message.length === 0;
-  };
-  
+  const normal_question = /[a-z0-9\D]+[?]$/gm;
+  const forceful_talking = /[a-z]+[!]$/gm;
+  const forceful_question = /[A-Z]+[?]$/gm;
+  const all_caps = /(\b[A-Z]+\b){3,}/gm;
+  const silence = message.length === 0;
+  switch (true) {
+    case normal_question.test(message):
+      return 'Sure.';
+    case forceful_talking.test(message):
+      return 'Whatever.';
+  }
+};
